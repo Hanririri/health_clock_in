5ac97f469d5cf9c0f331667d1e48da3fe182208b
Hanririri <49611440+Hanririri@users.noreply.github.com>
2021-07-25 11:09:13 +0800

    Update health_clock_in.py
    Since 7.24, no verification code required.

--- health_clock_in.py
+++ health_clock_in.py
@@ -1,10 +1,6 @@
 import time
 from selenium import webdriver
-# import requests
 from selenium.webdriver.common.action_chains import ActionChains
-# import pyautogui
-# import pytesseract
-# from PIL import Image
 
 driver = webdriver.Firefox()
 url = "http://yqtb.gzhu.edu.cn/infoplus/form/XNYQSB/start"
@@ -12,45 +8,27 @@ url = "http://yqtb.gzhu.edu.cn/infoplus/form/XNYQSB/start"
 while True:
   driver.get(url)
   time.sleep(2)
-  #code = driver.find_element_by_css_selector('img')
-  ## get picture
-  #action = ActionChains(driver).move_to_element(code)
-  #action.context_click(code)
-  #action.perform()
-  #pyautogui.typewrite(['v'])
-  #time.sleep(1)
-  #pyautogui.typewrite(['enter'])
-  #time.sleep(1)
-  #pyautogui.typewrite(['enter'])
-  
-  ## recognize picture
-  #time.sleep(10)
-  #img = Image.open("/home/hanri/Downloads/captcha.jsp.png")
-  #result = pytesseract.image_to_string(img)
-  #print(result)
 
   elem_user = driver.find_element_by_name("un")
-  elem_user.send_keys("<学号>")
+  elem_user.send_keys("<your_student_ID>")
   elem_pwd = driver.find_element_by_name("pd")
-  elem_pwd.send_keys("<密码>")
-  #elem_pwd = driver.find_element_by_name("captcha")
-  #elem_pwd.send_keys(result)
+  elem_pwd.send_keys("<your_password>")
   time.sleep(1)
   driver.find_element_by_xpath('//*[@id="index_login_btn"]').click() # login in
   time.sleep(6)
-  #enter
+  
+  driver.find_element_by_xpath('//*[@id="preview_start_button"]').click()
+  time.sleep(10)
+
   try:
-    driver.find_element_by_xpath('//*[@id="preview_start_button"]').click()
+    driver.find_element_by_xpath('//*[@id="V1_CTRL262"]').click()
   except:
     print('Error!')
-    time.sleep(5)
+    time.sleep(5) # restart
   else:
     print('success login in!')
     break
-  
-time.sleep(10)
-#enter
-driver.find_element_by_xpath('//*[@id="V1_CTRL262"]').click()
+
 time.sleep(1)
 driver.find_element_by_xpath('//*[@id="V1_CTRL82"]').click()
 time.sleep(1)
