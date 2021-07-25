c56b6c4e01f6c97e847868a922825643ae73edfd
Hanririri <49611440+Hanririri@users.noreply.github.com>
2021-07-25 10:54:30 +0800

    Update health_clock_in.py
    2021.7.25 don't need verification code.

--- health_clock_in.py
+++ health_clock_in.py
@@ -2,45 +2,46 @@ import time
 from selenium import webdriver
 import requests
 from selenium.webdriver.common.action_chains import ActionChains
-import pyautogui
-import pytesseract
-from PIL import Image
+# import pyautogui
+# import pytesseract
+# from PIL import Image
 
-# 模拟浏览器打开网站
+# \u6a21\u62df\u6d4f\u89c8\u5668\u6253\u5f00\u7f51\u7ad9
 driver = webdriver.Firefox()
 url = "http://yqtb.gzhu.edu.cn/infoplus/form/XNYQSB/start"
-# 将窗口最大化
+# \u5c06\u7a97\u53e3\u6700\u5927\u5316
 # browser.maximize_window()
-# get verification code to path:"/home/<user>/Downloads/captcha.jsp.png"
+# get verification code to path:"~/Downloads/captcha.jsp.png"
+
 while True:
   driver.get(url)
   time.sleep(2)
-  code = driver.find_element_by_css_selector('img')
-  # get picture
-  action = ActionChains(driver).move_to_element(code)
-  action.context_click(code)
-  action.perform()
-  pyautogui.typewrite(['v'])
-  time.sleep(1)
-  pyautogui.typewrite(['enter'])
-  time.sleep(1)
-  pyautogui.typewrite(['enter'])
+  #code = driver.find_element_by_css_selector('img')
+  ## get picture
+  #action = ActionChains(driver).move_to_element(code)
+  #action.context_click(code)
+  #action.perform()
+  #pyautogui.typewrite(['v'])
+  #time.sleep(1)
+  #pyautogui.typewrite(['enter'])
+  #time.sleep(1)
+  #pyautogui.typewrite(['enter'])
+  
+  ## recognize picture
+  #time.sleep(10)
+  #img = Image.open("/home/hanri/Downloads/captcha.jsp.png")
+  #result = pytesseract.image_to_string(img)
+  #print(result)
 
-  #recognize picture
-  time.sleep(10)
-  img = Image.open("/home/<user>/Downloads/captcha.jsp.png")
-  result = pytesseract.image_to_string(img)
-  print(result)
-
-  elem_user = driver.find_element_by_name("username")
-  elem_user.send_keys("<your_username>")
-  elem_pwd = driver.find_element_by_name("password")
-  elem_pwd.send_keys("<your_password>")
-  elem_pwd = driver.find_element_by_name("captcha")
-  elem_pwd.send_keys(result)
+  elem_user = driver.find_element_by_name("un")
+  elem_user.send_keys("1907400028")
+  elem_pwd = driver.find_element_by_name("pd")
+  elem_pwd.send_keys("5738387420")
+  #elem_pwd = driver.find_element_by_name("captcha")
+  #elem_pwd.send_keys(result)
   time.sleep(1)
-  driver.find_element_by_xpath('/html/body/div/div[1]/div/div/div[2]/div/form/div[5]/input[4]').click() # login in
-  time.sleep(10)
+  driver.find_element_by_xpath('//*[@id="index_login_btn"]').click() # login in
+  time.sleep(6)
   #enter
   try:
     driver.find_element_by_xpath('//*[@id="preview_start_button"]').click()
@@ -50,7 +51,7 @@ while True:
   else:
     print('success login in!')
     break
-
+  
 time.sleep(10)
 #enter
 driver.find_element_by_xpath('//*[@id="V1_CTRL262"]').click()
